18d5d5268d408d4ff19c48152183d5c54715c0c5
Sabrina Furtado <145471677+SabrinaFurtado@users.noreply.github.com>
2024-04-22 12:54:28 -0300

    Update Home.tsx

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import homeLogo from "C:/Users/sabri/OneDrive/Desktop/blog-pessoal-client/src/assets/sa.jpeg";
 import './Home.css';
 
 function Home() {
@@ -8,12 +7,12 @@ function Home() {
         <div className="bg-fuchsia-500 flex justify-center">
           <div className='container grid grid-cols-2 text-white'>
             <div className="flex flex-col gap-4 items-center justify-center py-4">
-              <h2 className='text-5xl font-light'>Bora lá postar algo!</h2>
-              <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
+              <h2 className='text-5xl font-light'>Comprar produtos de qualidade é aqui!</h2>
+              <p className='text-xl'>Aproveite!</p>
   
               <div className="flex justify-around gap-4">
               
-                <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
+                <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver Produtos</button>
               </div>
             </div>
   
